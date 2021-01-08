347a9316ea1f08bf30688ae1d29f19b465a9c676
simondfoster <simondf@gmail.com>
2021-01-08 21:59:18 +0900

    Factored out the ML and added some commentary to the development.

--- Expressions.thy
+++ Expressions.thy
@@ -1,7 +1,11 @@
+section \<open> Expressions \<close>
+
 theory Expressions
   imports Variables
   keywords "pretty_exprs" "full_exprs" "lit_vars" "expr_vars" "expr_ctr" :: "thy_decl_block"
 begin
+
+subsection \<open> Types and Constructs \<close>
 
 named_theorems expr_defs
 
@@ -12,17 +16,26 @@ text \<open> An expression is represented simply as a function from the state sp
   and a more intuitive expression syntax. For example, an expression @{term "x + y"} where 
   $x$ and $y$ are both state variables, can be represented by @{term "\<lambda> s. get\<^bsub>x\<^esub> s + get\<^bsub>y\<^esub> s"} 
   when both variables are modelled using lenses. Rather than having to write $\lambda$-terms 
-  directly, it is more convenient to hide this threading of state behind a parser. \<close>
+  directly, it is more convenient to hide this threading of state behind a parser.
+\<close>
 
 type_synonym ('a, 's) expr = "'s \<Rightarrow> 'a"
+
+text \<open> The following constructor is used to syntactically mark functions that actually
+  denote expressions. It is semantically vacuous. \<close>
 
 definition SEXP :: "('s \<Rightarrow> 'a) \<Rightarrow> ('a, 's) expr" ("[_]\<^sub>e") where
 [expr_defs]: "SEXP x = x"
 
 lemma SEXP_idem [simp]: "[[e]\<^sub>e]\<^sub>e = [e]\<^sub>e" by (simp add: SEXP_def)
 
+text \<open> We can create the core constructs of a simple expression language as indicated below. \<close>
+
 abbreviation (input) var :: "('a \<Longrightarrow> 's) \<Rightarrow> ('a, 's) expr" where
 "var x \<equiv> (\<lambda> s. get\<^bsub>x\<^esub> s)"
+
+abbreviation (input) lit :: "'a \<Rightarrow> ('a, 's) expr" where
+"lit k \<equiv> (\<lambda> s. k)"
 
 abbreviation (input) uop :: "('a \<Rightarrow> 'b) \<Rightarrow> ('a, 's) expr \<Rightarrow> ('b, 's) expr" where
 "uop f e \<equiv> (\<lambda> s. f (e s))"
@@ -31,7 +44,34 @@ abbreviation (input) bop
   :: "('a \<Rightarrow> 'b \<Rightarrow> 'c) \<Rightarrow> ('a, 's) expr \<Rightarrow> ('b, 's) expr \<Rightarrow> ('c, 's) expr" where
 "bop f e\<^sub>1 e\<^sub>2 \<equiv> (\<lambda> s. f (e\<^sub>1 s) (e\<^sub>2 s))"
 
+subsection \<open> Lifting Parser and Printer \<close>
+
+text \<open> The lifting parser creates a parser directive that converts an expression to a 
+  @{const SEXP} boxed $\lambda$-term that gives it a semantics. A pretty printer converts
+  a boxed $\lambda$-term back to an expression. \<close>
+
+ML_file \<open>Lift_Expr_Options.ML\<close>
+
+text \<open> We create a number of commands for configuring the way the parser works. \<close> 
+
+full_exprs
+pretty_exprs
+
+text \<open> We can disable pretty printing of $\lambda$ expressions using \textbf{full\_exprs} and
+  re-enable pretty printing with \textbf{pretty\_exprs}. \<close>
+
+lit_vars
+expr_vars
+
+text \<open> Expressions, of course, can contain variables. However, a variable can denote one of
+  three things: (1) a state variable (i.e. a lens); (2) a placeholder for a value (i.e. a
+  HOL literal); and (3) a placeholder for another expression. The command \textbf{lit\_vars}
+  selects option (2) as the default behaviour, and \textbf{expr\_vars} selects option (3). \<close>
+
 nonterminal sexp
+
+text \<open> Next, we create some syntactic constants and define parse and print translations for
+  them. \<close>
 
 syntax
   "_sexp_state" :: "id"
@@ -41,149 +81,18 @@ syntax
   "_sexp_evar"  :: "id_position \<Rightarrow> logic" ("@_" [999] 999)
   "_sexp_pqt"   :: "logic \<Rightarrow> sexp" ("[_]\<^sub>e")
 
-
-ML \<open>
-val state_id = "\<s>";
-\<close>
+ML_file \<open>Lift_Expr.ML\<close>
 
 parse_translation \<open> 
-  [(@{syntax_const "_sexp_state"}, fn ctx => fn term => Syntax.free state_id)]
-\<close>
-
-translations
-(*  "_sexp_lit x" => "x" *)
-  "_sexp_var x" => "get\<^bsub>x\<^esub> _sexp_state"
-(*  "_sexp_evar e" => "e _sexp_state" *)
-
-ML \<open>
-structure LitVars = Theory_Data
-  (type T = bool
-   val empty = false
-   val extend = I
-   val merge = (fn (x, y) => x orelse y));
-
-Outer_Syntax.command @{command_keyword "lit_vars"} "treat variables as literals by default" 
-  (Scan.succeed (Toplevel.theory (LitVars.put true)));
-
-Outer_Syntax.command @{command_keyword "expr_vars"} "treat variables as expressions by default" 
-  (Scan.succeed (Toplevel.theory (LitVars.put false)));
-
-structure FullExprs = Theory_Data
-  (type T = bool
-   val empty = false
-   val extend = I
-   val merge = (fn (x, y) => x orelse y));
-
-Outer_Syntax.command @{command_keyword "full_exprs"} "print full form of expressions" 
-  (Scan.succeed (Toplevel.theory (FullExprs.put true)));
-
-Outer_Syntax.command @{command_keyword "pretty_exprs"} "pretty print expressions" 
-  (Scan.succeed (Toplevel.theory (FullExprs.put false)));
-
-structure NoLift = Theory_Data
-  (type T = unit Symtab.table
-   val empty = Symtab.empty
-   val extend = I
-   val merge = Symtab.merge (K true));
-
-fun nolift_const thy n =  
-          let val Const (c, _) = Proof_Context.read_const {proper = true, strict = false} (Proof_Context.init_global thy) n 
-          in NoLift.map (Symtab.update (c, ())) thy end;
-
-Outer_Syntax.command @{command_keyword "expr_ctr"} "declare that certain constants should not be lifted"
-    (Scan.repeat1 (Parse.term)
-     >> (fn ns => 
-         Toplevel.theory 
-         (fn thy => Library.foldl (fn (thy, n) => nolift_const thy n) (thy, ns))));
-
-
-Outer_Syntax.command @{command_keyword "full_exprs"} "print full form of expressions" 
-
-\<close>
-
-ML \<open> 
-fun lift_expr_aux ctx (Const (n', t), args) =
-  let 
-    open Syntax
-    val n = (if (Lexicon.is_marked n') then Lexicon.unmark_const n' else n')
-    val args' = map (lift_expr ctx) args
-  in if (n = @{const_name lens_get} orelse n = @{const_name SEXP}) 
-     then Term.list_comb (Const (n', t), args)
-     else if (n = @{syntax_const "_sexp_lit"})
-     then Term.list_comb (hd args, tl args')
-     else if (n = @{syntax_const "_sexp_evar"})
-     then Term.list_comb (hd args $ free state_id, tl args')
-     else if (member (op =) (Symtab.keys (NoLift.get (Proof_Context.theory_of ctx))) n)
-     then Term.list_comb (Const (n', t), args) $ free state_id
-     else
-     case (Type_Infer_Context.const_type ctx n) of
-      SOME (Type (\<^type_name>\<open>lens_ext\<close>, _)) 
-        => Term.list_comb (const @{const_name lens_get} $ Const (n', t) $ free state_id, args') |
-      _ => Term.list_comb (Const (n, t), args')
-  end |
-lift_expr_aux ctx (Free (n, t), args) = 
-    let val consts = (Proof_Context.consts_of ctx)
-        val {const_space, ...} = Consts.dest consts
-        \<comment> \<open> The name must be internalised in case it needs qualifying. \<close>
-        val c = Consts.intern consts n in
-        \<comment> \<open> If the name refers to a declared constant, then we lift it as a constant. \<close>
-        if (Name_Space.declared const_space c) then
-          lift_expr_aux ctx (Const (c, t), args)
-        \<comment> \<open> Otherwise, we leave it alone \<close>
-        else
-          let val trm = if (LitVars.get (Proof_Context.theory_of ctx)) 
-                        then Free (n, t) else Free (n, t) $ Syntax.free state_id
-          in Term.list_comb (trm, map (lift_expr ctx) args) end
-  end |
-lift_expr_aux ctx (e, args) = Term.list_comb (e, args)
-and 
-lift_expr ctx (Abs (n, t, e)) = 
-  if (n = state_id) then Abs (n, t, e) else Abs (n, t, lift_expr ctx e) |
-lift_expr ctx e = lift_expr_aux ctx ((Term.strip_comb e))
-\<close>
-
-parse_translation \<open>
-  [(@{syntax_const "_sexp_quote"}
+  [(@{syntax_const "_sexp_state"}, fn ctx => fn term => Syntax.free Lift_Expr.state_id),
+   (@{syntax_const "_sexp_quote"}
    , fn ctx => fn terms =>
       case terms of
         [Const (@{const_syntax SEXP}, t) $ e] => Const (@{const_name SEXP}, t) $ e |
         [e] =>
             Syntax.const @{const_name SEXP} 
-            $ (lambda (Syntax.free state_id) (lift_expr ctx (Term_Position.strip_positions e))))]
-\<close>
-
-ML \<open> \<close>
-
-ML \<open> 
-tl;
-
-fun print_expr_aux ctx (f, args) =
-  let open Proof_Context
-      fun sexp_evar x = if (LitVars.get (theory_of ctx)) then Syntax.const "_sexp_evar" $ x else x
-  in
-  case (f, args) of
-    (Const (@{syntax_const "_free"}, _), (Free (n, t) :: Const (@{syntax_const "_sexp_state"}, _) :: r)) => 
-     sexp_evar (Term.list_comb (Syntax.const @{syntax_const "_free"} $ Free (n, t)
-                              , map (print_expr ctx) r)) |
-    (Const (@{const_syntax lens_get}, _), [x, Const (@{syntax_const "_sexp_state"}, _)])
-      => Syntax.const @{syntax_const "_sexp_var"} $ x |
-    (Free (n, t), [Const (@{syntax_const "_sexp_state"}, _)]) 
-      => sexp_evar (Free (n, t)) | 
-    _ => if (length args > 0)
-         then case (List.last args) of
-           Const (@{syntax_const "_sexp_state"}, _) => 
-             Term.list_comb (f, map (print_expr ctx) (take (length args - 1) args)) |
-           _ => Term.list_comb (f, map (print_expr ctx) args)
-         else Term.list_comb (f, map (print_expr ctx) args)
-   end
-and 
-print_expr ctx (Abs (n, t, e)) = Abs (n, t, print_expr ctx e) |
-print_expr ctx e = print_expr_aux ctx (Term.strip_comb e)
-
-fun strip_sexp_state (f, args) =
-  let val args' = filter (fn x => case x of Const (@{syntax_const "_sexp_state"}, _) => false | _ => true) args
-      val args'' = map (strip_sexp_state o Term.strip_comb) args'
-  in Term.list_comb (f, args'') end;
+            $ (lambda (Syntax.free Lift_Expr.state_id) 
+                      (Lift_Expr.lift_expr ctx (Term_Position.strip_positions e))))]
 \<close>
 
 print_translation \<open>
@@ -193,33 +102,71 @@ print_translation \<open>
      then Syntax.const @{syntax_const "_sexp_pqt"} $ hd ts 
      else
      Syntax.const @{syntax_const "_sexp_quote"} 
-     $ print_expr ctx (betapply ((hd ts), Syntax.const @{syntax_const "_sexp_state"})))]
+     $ Lift_Expr.print_expr ctx (betapply ((hd ts), Syntax.const @{syntax_const "_sexp_state"})))]
 \<close>
+
+translations
+  "_sexp_var x" => "get\<^bsub>x\<^esub> _sexp_state"
+
+text \<open> The main directive is the $e$ subscripted brackets, @{term "(e)\<^sub>e"}. This converts the 
+  expression $e$ to a boxed $\lambda$ term. Essentially, the parser behaviour is as follows:
+
+  \begin{enumerate}
+    \item a new $\lambda$ abstraction over the state variable $s$ is wrapped around $e$;
+    \item every occurrence of free lens @{term "&x"} in $e$ is replace by @{term "get\<^bsub>x\<^esub> s"};
+    \item every occurrence of an expression variable @{term "e"} is replaced by @{term "e s"}.
+  \end{enumerate}
+
+  The pretty printer does this in reverse. Some examples follow. For now, we turn of the 
+  pretty printer so that we can see the results of the parser.
+\<close>
+
+full_exprs
+
+term "(f + g)\<^sub>e"
+
+text \<open> The default behaviour of our parser is to recognise identifiers as expression variables.
+  So, the above expression becomes the term @{term "[\<lambda>\<s>. f \<s> + g \<s>]\<^sub>e"}. We can easily change
+  this: \<close>
 
 lit_vars
 
-term "(f \<s> + \<guillemotleft>x\<guillemotright>)\<^sub>e"
+term "(f + g)\<^sub>e"
 
-full_exprs
+text \<open> Now, @{term f} and @{term g} are both parsed as literals, and so the term is 
+  @{term "[\<lambda>\<s>. f + g]\<^sub>e"}. Alternatively, we could have a lens in the expression: \<close>
+
+term "(&x + g)\<^sub>e"
+
+text \<open> This gives the term @{term "[\<lambda>\<s>. get\<^bsub>x\<^esub> \<s> + g]\<^sub>e"}. Although we have default behaviours
+  for parsing, we can use different markup to coerce identifiers to particular variable kinds. \<close>
+
+term "(&x + @g)\<^sub>e"
+
+text \<open> This gives @{term "[\<lambda>\<s>. get\<^bsub>x\<^esub> \<s> + g \<s>]\<^sub>e"}, the we have requested that @{term "g"} is 
+  treated as an expression variable. We can do similar with literal, as show below. \<close>
+
+term "(f + \<guillemotleft>x\<guillemotright>)\<^sub>e"
+
+text \<open> Some further examples follow. \<close>
 
 term "(\<guillemotleft>f\<guillemotright> (@e))\<^sub>e"
 
-term "(f)\<^sub>e"
+term "(@f + @g)\<^sub>e"
 
-expr_vars
 
-full_exprs
-
-term "SEXP(\<lambda> \<s>. get\<^bsub>x\<^esub> \<s> + e \<s> + v)"
-
-pretty_exprs
-expr_vars
 
 term "(@x)\<^sub>e"
 
 term "SEXP(\<lambda> \<s>. get\<^bsub>x\<^esub> \<s> + e \<s> + v)"
 
-lemma "(v \<in> (&xs) \<union> (&f) ys \<union> {} \<and> @e)\<^sub>e = undefined"
-  apply (simp) oops
+term "(v \<in> (&xs) \<union> (&f) ys \<union> {} \<and> @e)\<^sub>e"
+
+pretty_exprs
+expr_vars
+
+text \<open> The pretty printer works even when we don't use the parser, as shown below. \<close>
+
+term "[\<lambda> \<s>. get\<^bsub>x\<^esub> \<s> + e \<s> + v]\<^sub>e"
 
 end
