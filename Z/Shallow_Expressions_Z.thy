3a2e9a3f0cfed35e15a9b4105f7d77a52b1b985d
Simon Foster <simon.foster@york.ac.uk>
2021-08-25 13:32:46 +0100

    Added more imports to meta-theory

--- Z/Shallow_Expressions_Z.thy
+++ Z/Shallow_Expressions_Z.thy
@@ -1,5 +1,8 @@
 section \<open> Shallow Expressions with Z \<close>
 
 theory Shallow_Expressions_Z
-  imports Collections_Z
+  imports 
+    "Shallow-Expressions.Shallow_Expressions" 
+    "Z_Toolkit.Relation_Lib"
+    Collections_Z
 begin end
