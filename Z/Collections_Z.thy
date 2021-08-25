c667f4c3c9151e433e2b6122f0437b98a5f05e18
Simon Foster <simon.foster@york.ac.uk>
2021-08-25 13:19:28 +0100

    Added a session integrating shallow expressions and Z

---
+++ Z/Collections_Z.thy
@@ -0,0 +1,43 @@
+section \<open> Collections from Z Toolkit \<close>
+
+theory Collections_Z
+  imports "Shallow-Expressions.Collections" "Z_Toolkit.Relation_Lib"
+begin
+
+subsection \<open> Partial Function Collection Lens \<close>
+
+definition pfun_collection_lens :: "'a \<Rightarrow> 'b \<Longrightarrow> 'a \<Zpfun> 'b" where
+[lens_defs]: "pfun_collection_lens = pfun_lens"
+
+adhoc_overloading collection_lens pfun_collection_lens
+
+lemma pfun_collection_lens_mwb [simp]: "mwb_lens (pfun_collection_lens e)"
+  by (simp add: pfun_collection_lens_def)
+
+lemma source_pfun_collection_lens: "\<S>\<^bsub>pfun_collection_lens i\<^esub> = {f. i \<in> pdom(f)}"
+  by (auto simp add: lens_defs lens_source_def, metis pfun_upd_ext)
+
+lemma defined_pfun_collection_lens [simp, code_unfold]: 
+  "\<lbrakk> vwb_lens x; $x \<sharp> (e)\<^sub>e \<rbrakk> \<Longrightarrow> \<^bold>D(x[e]) = (e \<in> pdom($x))\<^sub>e"
+  by (simp add: lens_defined_def src_dyn_lens unrest source_ns_alpha source_pfun_collection_lens)
+     (simp add: lens_defs wb_lens.source_UNIV)
+
+subsection \<open> Finite Function Collection Lens \<close>
+
+definition ffun_collection_lens :: "'a \<Rightarrow> 'b \<Longrightarrow> 'a \<Zffun> 'b" where
+[lens_defs]: "ffun_collection_lens = ffun_lens"
+
+adhoc_overloading collection_lens ffun_collection_lens
+
+lemma ffun_collection_lens_mwb [simp]: "mwb_lens (ffun_collection_lens e)"
+  by (simp add: ffun_collection_lens_def)
+
+lemma source_ffun_collection_lens: "\<S>\<^bsub>ffun_collection_lens i\<^esub> = {f. i \<in> fdom(f)}"
+  by (auto simp add: lens_defs lens_source_def, metis ffun_upd_ext)
+
+lemma defined_ffun_collection_lens [simp, code_unfold]: 
+  "\<lbrakk> vwb_lens x; $x \<sharp> (e)\<^sub>e \<rbrakk> \<Longrightarrow> \<^bold>D(x[e]) = (e \<in> fdom($x))\<^sub>e"
+  by (simp add: lens_defined_def src_dyn_lens unrest source_ns_alpha source_ffun_collection_lens)
+     (simp add: lens_defs wb_lens.source_UNIV)
+
+end
---
+++ Z/Shallow_Expressions_Z.thy
@@ -0,0 +1,5 @@
+section \<open> Shallow Expressions with Z \<close>
+
+theory Shallow_Expressions_Z
+  imports Collections_Z
+begin end
