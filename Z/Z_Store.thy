18e9e862eaad47f3b40b435a8a32860194fe0554
Simon Foster <simon.foster@york.ac.uk>
2022-01-17 15:02:53 +0000

    Added the zexpr command, which allows definition of expressions over a zstore

--- Z/Z_Store.thy
+++ Z/Z_Store.thy
@@ -2,7 +2,7 @@ section \<open> Z Stores \<close>
 
 theory Z_Store
   imports "Shallow-Expressions.EDefinitions" "Z_Toolkit.Relation_Lib"
-  keywords "zstore" :: "thy_decl_block"
+  keywords "zstore" "zexpr" :: "thy_decl_block"
 begin
 
 text \<open> This theory creates a command for adding Z-like state schemas. It creates both an alphabet,
@@ -10,7 +10,7 @@ text \<open> This theory creates a command for adding Z-like state schemas. It c
   a set of assumptions. \<close>
 
 named_theorems z_defs and z_locale_defs
-
+               
 ML_file \<open>Z_Store.ML\<close>
 
 end
