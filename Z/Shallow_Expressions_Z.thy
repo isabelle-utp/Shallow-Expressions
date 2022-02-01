f916045719d1d3caa990c02608df670278853476
Simon Foster <simon.foster@york.ac.uk>
2022-02-01 12:36:00 +0000

    Added proof automation for the refinement operator on predicates

--- Z/Shallow_Expressions_Z.thy
+++ Z/Shallow_Expressions_Z.thy
@@ -13,4 +13,7 @@ text \<open> Allow substitution maplets to be written in a Z-like way. \<close>
 syntax "_zmaplet" :: "[svid, logic] => smaplet" ("_\<Zprime> = _")
 translations "_zmaplet x e" => "_smaplet x e"
 
+lemma ref_by_pred_iff [expr_simps]: "P \<sqsubseteq> Q \<longleftrightarrow> `Q \<longrightarrow> P`"
+  by (simp add: ref_by_bool_def ref_by_fun_def taut_def) 
+
 end
